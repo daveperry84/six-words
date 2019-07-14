03262d39a03153577590c126c3aae40c20907945
Dave Perry <daveperry84@hotmail.co.uk>
2019-07-14 13:22:07 +0100

    Adding routing, a home page and some design ideas to the home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const ROUTES: Route[] = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
