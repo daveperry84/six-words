fee22f0c418e604ec30a526e5e6334e399d889fb
Dave Perry <daveperry84@hotmail.co.uk>
2019-07-29 21:43:56 +0100

    set time back to 3 minutes

--- src/app/shared/services/game-timer.service.ts
+++ src/app/shared/services/game-timer.service.ts
@@ -2,7 +2,7 @@ import { Observable, BehaviorSubject } from "rxjs";
 import { IGameTimer } from "../types/game-timer.interface";
 
 export class GameTimerService {
-  private _gameTimerSubject: BehaviorSubject<IGameTimer> = new BehaviorSubject<IGameTimer>({ minutes: 0, seconds: 10 });
+  private _gameTimerSubject: BehaviorSubject<IGameTimer> = new BehaviorSubject<IGameTimer>({ minutes: 3, seconds: 0 });
   private _gameStartedSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private _gameEndedSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private _timer: any;
