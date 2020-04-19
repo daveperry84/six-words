d40c0c6e0c67887c9fffd9e16fa243b9370012a9
daveperry84 <daveperry84@hotmail.co.uk>
2020-04-19 18:40:20 +0100

    Update random-letter.service.ts

--- src/app/services/random-letter.service.ts
+++ src/app/services/random-letter.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class RandomLetterService {
   private _letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
